99f820c470aa2619e34cd2719954171636ef828e
Ankit Prasad <ankit777666@gmail.com>
2023-11-24 16:53:17 +0530

    Count Smaller elements

---
+++ gfg/Count Smaller elements.cpp
@@ -0,0 +1,22 @@
+class Solution{
+public:	
+	vector<int> constructLowerArray(int *arr, int n) {
+	    // code here
+	    vector<int> ans;
+	    vector<int> temp;
+	    
+	    for(int i=0; i<n; i++) {
+	        temp.push_back(arr[i]);
+	    }
+	    sort(temp.begin(), temp.end());
+	    
+	    for(int i=0; i<n; i++) {
+	        int index = lower_bound(temp.begin(), temp.end(), arr[i])-temp.begin();
+	        ans.push_back(index);
+	        
+	        temp.erase(temp.begin()+index);
+	    }
+	    return ans;
+	    
+	}
+};
