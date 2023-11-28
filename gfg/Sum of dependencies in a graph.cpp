4785f75051b25c5538c94e4b4d5595251e411ede
Ankit Prasad <ankit777666@gmail.com>
2023-11-28 12:18:36 +0530

    Sum of dependencies in a graph

---
+++ gfg/Sum of dependencies in a graph.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int sumOfDependencies(vector<int> adj[], int V) {
+        // code here
+        int ans = 0;
+        
+        for(int i=0; i<V; i++)
+            ans+=adj[i].size();
+        
+        return ans;
+    }
+};
