d93e1fb5e2a069f89a6eb39255a1429b5b63de07
Ankit Prasad <ankit777666@gmail.com>
2023-11-28 19:19:57 +0530

    Check if strings are rotations of each other or not

---
+++ gfg/Check if strings are rotations of each other or not.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        if(s1.size() != s2.size())
+            return false;
+        s1+=s1;
+        if(s1.find(s2) != -1)
+            return true;
+        return false;
+    }
+    
+};
