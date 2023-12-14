1f939e891f3a68947143d89ec7a6e717b8f0213c
Ankit Prasad <ankit777666@gmail.com>
2023-12-15 00:37:39 +0530

    Water Collection

---
+++ gfg/Water Collection.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxWater(int arr[], int n) {
+            // code here 
+        int left[n];
+        int right[n];
+        left[0] = arr[0];
+        right[n-1] = arr[n-1];
+        
+        int water = 0;
+        
+        for(int i=1;i<n;i++){
+            left[i] = max(left[i-1],arr[i]);
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            right[i] = max(right[i+1],arr[i]);
+        }
+        
+        for(int i=0;i<n;i++){
+            water += (min(left[i],right[i]) - arr[i]);
+        }
+        
+        return water;
+    }
+};
