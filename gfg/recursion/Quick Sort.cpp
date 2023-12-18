f44935a901af6846b1e3e3b65517184567beb706
Ankit Prasad <ankit777666@gmail.com>
2023-12-18 15:04:47 +0530

    Quick Sort

---
+++ gfg/recursion/Quick Sort.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low<high) {
+            int pIndex=partition(arr, low, high);
+            quickSort(arr, low, pIndex-1);
+            quickSort(arr, pIndex+1, high);
+        }
+        
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       // Your code here
+       int pivot=arr[low];
+       int i=low, j=high;
+       while(i<j) {
+           while(arr[i]<=pivot && i<=high-1) {
+               i++;
+           }
+           while(arr[j]>pivot && j>=low+1) {
+               j--;
+           }
+           if(i<j) 
+                swap(arr[i], arr[j]);
+            
+       }
+       swap(arr[low], arr[j]);
+       return j;
+    }
+};
