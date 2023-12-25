19f3e60d901a61630fb8cad8dcf70c6b21d792cc
Ankit Prasad <ankit777666@gmail.com>
2023-12-26 00:25:36 +0530

    another approach

--- leetCode/backtracking/79. Word Search.cpp
+++ leetCode/backtracking/79. Word Search.cpp
@@ -34,3 +34,43 @@ public:
         
     }
 };
+
+//Another approach
+
+class Solution {
+public:
+    bool existUtil(int i, int j,int index,vector<vector<char>>&board,string &word) {
+        if(index==word.size()-1)
+            return true;
+        // visited[i][j]=true;
+        char temp=board[i][j];
+        board[i][j]='_';
+        if(i+1<board.size() && board[i+1][j]==word[index+1] && existUtil(i+1,j, index+1,board,word))
+            return true;
+        if(i-1>=0 && board[i-1][j]==word[index+1] && existUtil(i-1,j, index+1,board,word))
+            return true;
+        if(j+1<board[0].size() && board[i][j+1]==word[index+1] && existUtil(i,j+1, index+1,board,word))
+            return true;
+        if(j-1>=0 && board[i][j-1]==word[index+1] && existUtil(i,j-1, index+1,board,word))
+            return true;
+        board[i][j]=temp;
+        // visited[i][j]=false;
+        return false;
+
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int index=0;
+        int n=board.size(), m=board[0].size();
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(board[i][j]==word[0] && existUtil(i,j, index,board,word)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+        
+    }
+};
