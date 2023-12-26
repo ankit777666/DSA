bb6649c331afbbbff81f8db2930aba003d48a2e1
Ankit Prasad <ankit777666@gmail.com>
2023-12-26 14:02:42 +0530

    113. Path Sum II

---
+++ leetCode/backtracking/113. Path Sum II.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void pathSumUtil(TreeNode* root, int targetSum, vector<int>&v,vector<vector<int>>&ans) {
+        if(root==NULL) return;
+        if(root->left ==NULL && root->right==NULL) {
+            if(targetSum==root->val) {
+                v.push_back(root->val);
+                ans.push_back(v);
+                v.pop_back();
+                return;
+            }
+        }
+        
+        v.push_back(root->val);
+        pathSumUtil(root->left, targetSum-root->val, v, ans);
+        pathSumUtil(root->right, targetSum-root->val, v, ans);
+        v.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>>ans;
+        vector<int>v;
+        pathSumUtil(root, targetSum, v, ans);
+        return ans;
+    }
+};
