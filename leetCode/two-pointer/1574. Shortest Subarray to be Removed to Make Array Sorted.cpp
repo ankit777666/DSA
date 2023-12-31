187dbdae1c88fbcba6c42f390fd1a6b117be7cfb
Ankit Prasad <ankit777666@gmail.com>
2023-12-31 09:08:13 +0530

    1574. Shortest Subarray to be Removed to Make Array Sorted

---
+++ leetCode/two-pointer/1574. Shortest Subarray to be Removed to Make Array Sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n=arr.size();
+        int left=0, right=n-1;
+        while(left+1<n && arr[left]<=arr[left+1])
+            left++;
+        if(left==n-1) return 0;
+        while(right>left && arr[right]>=arr[right-1])
+            right--;
+        int ans=min(n-left-1,right);
+        int i=0, j=right;
+        while(i<=left && j<n) {
+            if(arr[j]>=arr[i]) {
+                ans=min(ans, j-i-1);
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
