613fbcf796f80181095cb0fe79d2d949bcc8b992
Ankit Prasad <ankit777666@gmail.com>
2024-01-04 16:42:56 +0530

    275. H-Index II

---
+++ leetCode/binary-search/275. H-Index II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        int low=0, high=n-1;
+
+        int ans=0;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(citations[mid]>=n-mid) {
+                ans=n-mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
