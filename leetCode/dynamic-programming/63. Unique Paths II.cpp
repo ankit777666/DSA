9b748432bd5266b2c0cc730437b5aa19fab2f013
Ankit Prasad <ankit777666@gmail.com>
2024-01-17 02:28:50 +0530

    //memoization

--- leetCode/dynamic-programming/63. Unique Paths II.cpp
+++ leetCode/dynamic-programming/63. Unique Paths II.cpp
@@ -51,3 +51,25 @@ public:
         return util(m-1, n-1, m,n,obstacleGrid);
     }
 };
+//memoization
+class Solution {
+public:
+    int util(int i, int j, int m, int n,vector<vector<int>>& obstacleGrid, vector<vector<int>>&dp) {
+        if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
+            return 0;
+        if(i==0 && j==0)
+            return 1;
+        if(i<0 || j<0)
+            return 0;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=util(i-1,j,m,n,obstacleGrid,dp)+util(i,j-1,m,n,obstacleGrid,dp);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        return util(m-1, n-1, m,n,obstacleGrid,dp);
+    }
+};
