d0ef46a164f14a298160273821a31e76c6454d46
Ankit Prasad <ankit777666@gmail.com>
2024-07-01 17:50:29 +0530

    33. Search in Rotated Sorted Array

--- leetCode/binary-search/33. Search in Rotated Sorted Array.cpp
+++ leetCode/binary-search/33. Search in Rotated Sorted Array.cpp
@@ -24,4 +24,40 @@ public:
         }
         return -1;
     }
+
+};
+/**
+ * same approach, differnt dry run
+ */
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low= 0, high = nums.size()-1;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[high]>=nums[mid]) {
+                if(nums[mid]<=target && nums[high]>=target) {
+                    low = mid+1;
+                }
+                else {
+                    high=mid-1;
+                }
+            }
+            else {
+                if(nums[low]<=target && nums[mid]>=target) {
+                    high=mid-1;
+                }
+                else {
+                    low=mid+1;
+                }
+            }
+
+
+        }
+        return -1;
+
+    }
 };
