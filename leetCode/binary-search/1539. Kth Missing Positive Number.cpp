a4e043e2a4b81300d4c4a0b76f4ae392516133b5
Ankit Prasad <ankit777666@gmail.com>
2024-07-04 22:39:34 +0530

    1539. Kth Missing Positive Number

---
+++ leetCode/binary-search/1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n=arr.size();
+        int low=0, high=n-1;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            int diff=arr[mid]-(mid+1);
+            if(diff<k)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return k+high+1;
+    }
+};
