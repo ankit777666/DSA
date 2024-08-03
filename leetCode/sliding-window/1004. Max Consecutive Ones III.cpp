eb2a9081f072c5fc6e327139052396439a784408
Ankit Prasad <ankit777666@gmail.com>
2024-08-04 03:39:10 +0530

    1004. Max Consecutive Ones III

--- leetCode/sliding-window/1004. Max Consecutive Ones III.cpp
+++ leetCode/sliding-window/1004. Max Consecutive Ones III.cpp
@@ -20,3 +20,27 @@ public:
         return maxLen;
     }
 };
+
+// optimized - beats 86%
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n=nums.size();
+        int i=0, j=0;
+        int zeroCount=0;
+        int maxLen=INT_MIN;
+
+        while(j<n) {
+          if(nums[j]==0)
+            zeroCount++;
+          while(zeroCount>k) {
+            if(nums[i]==0)
+              zeroCount--;
+            i++;
+          }
+          maxLen=max(maxLen, j-i+1);
+          j++;
+        }
+        return maxLen;
+    }
+};
